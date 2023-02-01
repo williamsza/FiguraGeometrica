510e5f9751d2706583a4dc075bf17c87ff6f1c8b
williamsza <william.sza@outlook.com>
2023-02-01 14:47:48 -0300

    Atualização

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Circulo circulo = new Circulo(2);
-        System.out.println("A área do ciculo é %2.d: " + circulo.calcularArea());
+        System.out.println("A área do ciculo é: " + circulo.calcularArea());
 
         Quadrado quadrado = new Quadrado(5);
         System.out.println("A área do qudrado é: " + quadrado.calcularArea());
