0017be6fb1a1046e954b6d323819156fb76bec67
williamsza <william.sza@outlook.com>
2023-02-02 13:52:36 -0300

    Alterações

--- src/App.java
+++ src/App.java
@@ -1,10 +1,19 @@
+import java.io.IOException;
+
 import classes.Circulo;
 import classes.Quadrado;
 
 public class App {
-    public static void main(String[] args) throws Exception {
 
-        Circulo circulo = new Circulo(2);
+    public static void limparTela() throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        limparTela();
+
+        Circulo circulo = new Circulo(10);
         System.out.println("A área do ciculo é: " + circulo.calcularArea());
 
         Quadrado quadrado = new Quadrado(5);
